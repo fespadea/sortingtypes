8406ae9a52bb71779449efcbf99f21d93ca922cd
fspadea <fs279@pvsd.org>
2017-02-08 14:30:46 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int theArray[] = {42, 23, 16, 15, 8, 4};
+
+        sort(theArray);
+
+        for(int j = 0; j < theArray.length; j++) {
+
+            System.out.print(theArray[j] + " "); }
+
+        System.out.println(" "); }
+
+    public static void sort(int a[ ]){
+        for(int x = 0; x < a.length-1; x++){
+            for(int y = 0; y < a.length-1; y++){
+                
+            }
+        }
+    }
+}
